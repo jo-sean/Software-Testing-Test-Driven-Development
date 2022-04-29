1ec09bfa175bbc5f929ad5d410da44ab6dede38d
jo-sean <joseanmanuel@gmail.com>
2022-04-29 11:11:32 -0700

    Initialized an empty test suite and a barebones check_pwd func

---
+++ check_pwd.py
@@ -0,0 +1,7 @@
+# Name: Sean Perez
+# CS 362: A2
+# Date: 4.29.22
+
+
+def check_pwd():
+    return True
---
+++ tests.py
@@ -0,0 +1,13 @@
+# Name: Sean Perez
+# CS 362: A2
+# Date: 4.29.22
+
+import unittest
+from check_pwd import check_pwd
+
+
+class TestCase(unittest.TestCase):
+    """TDD implementation"""
+
+    def test1(self):
+        pass
