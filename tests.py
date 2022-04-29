473942fe86b18635eb719082a534c8cb068e390b
jo-sean <joseanmanuel@gmail.com>
2022-04-29 15:03:17 -0700

    Wrote test3 to see if string of character length 21 is rejected correctly

--- tests.py
+++ tests.py
@@ -9,12 +9,20 @@ from check_pwd import check_pwd
 class TestCase(unittest.TestCase):
     """TDD implementation"""
 
+    # Empty string
     def test1(self):
         test_pwd = ''
         self.assertFalse(check_pwd(test_pwd),
                          msg=''.format(check_pwd(test_pwd)))
 
+    # Character length equals 7
     def test2(self):
         test_pwd = 'aj7fg45'
+        self.assertFalse(check_pwd(test_pwd),
+                         msg=''.format(check_pwd(test_pwd)))
+
+    # Character length equals 21
+    def test3(self):
+        test_pwd = 'aj7fg57fhe7trf74jds92'
         self.assertFalse(check_pwd(test_pwd),
                          msg=''.format(check_pwd(test_pwd)))
